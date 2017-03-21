3ef47e55a685394d21e3e296b3237d23c86a9250
邹宇绅 <335498421@qq.com>
2017-03-21 22:35:39 +0800

    添加 ng表单处理

---
+++ components/ng表单处理/ngform.js
@@ -0,0 +1,52 @@
+var app = angular.module('app', []);
+
+// 控制器
+app.controller('ngformController', ['$scope', '$http', function($scope, $http){
+    // 控制该页面交互的对象
+    $scope.ngformData = {};
+
+    // 页面初始化
+    initNgFormFn();
+
+    function initNgFormFn() {
+        // 获取兴趣数组
+        $http.get('ngform.json').success(function(res){
+            if(res.code === 0) {
+                $scope.ngformObj  = {
+                    interests: res.interesting
+                }
+            }
+        }).error(function(res){
+
+        });
+    }
+
+    $scope.ngformData.selectid = [];
+    $scope.ngformData.selectname = [];
+    // ----------------------点击复选框
+    $scope.select = function($event){
+        var selectid = $scope.ngformData.selectid;
+        var selectname = $scope.ngformData.selectname;
+        var target = $event.target,
+            ischecked = target.checked,
+            action = ischecked ? 'add' : 'remove',
+            id = target.id,
+            name = target.name,
+            idx = selectid.indexOf(id);
+        if(action === 'add' && idx === -1) {
+            selectid.push(id);
+            selectname.push(name);
+        } else if (action === 'remove' && idx > -1){
+            selectid.splice(idx, 1);
+            selectname.splice(idx, 1);
+        }
+        console.log(selectid);
+        console.log(selectname);
+    };
+    // ----------------------点击复选框 end
+
+    // 提交
+    $scope.submit = function(){
+        console.log($scope.ngformData);
+    };
+}]);

123884d321c7b582c36fe45c644a9a7703cc4819
邹宇绅 <335498421@qq.com>
2017-03-26 17:08:56 +0800

    增加注释

--- components/ng实例/js/config.js
+++ components/ng实例/js/config.js
@@ -1,16 +1,18 @@
 var app = angular.module('app', ['ui.router']);
 
+// 路由
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    // 设置默认地址
     $urlRouterProvider.otherwise('/first');
 
     $stateProvider
-    .state('first', {
-        url: '/first',
+    .state('first', { // $state.go()
+        url: '/first', // $location.url()
         pageTitle: '第一页',
         templateUrl: './first.html'
     })
     .state('second', {
-        url: '/second?second_params',
+        url: '/second?second_params', // 在这里设置形参，多个就  xxx?a&b&c
         pageTitle: '第二页',
         templateUrl: './second.html'
     })
@@ -18,7 +20,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         url: '/third?third_params',
         pageTitle: '第三页',
         templateUrl: './third.html',
-        controller: function($scope, $stateParams){
+        controller: function($scope, $stateParams){ // 跟ng-controller一样，这里的controller启动完毕后才到ng-controller那边启动
             $scope.third_params = $stateParams.third_params;
         }
     })

432dfec0ae0fcf80381227177899c7765f5263fc
If <247596814@qq.com>
2018-03-13 18:11:50 +0800

    update

--- components/vue-cli/.eslintrc.js
+++ components/vue-cli/.eslintrc.js
@@ -3,7 +3,8 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   env: {
     browser: true,
@@ -13,12 +14,12 @@ module.exports = {
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'eslint:recommended'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  //   plugins: [
+  //     'vue'
+  //   ],
   // add your custom rules here
   rules: {
     // allow async-await
